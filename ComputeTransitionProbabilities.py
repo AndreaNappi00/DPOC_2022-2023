8b2be0dcde6eb02db429675a28246ae86ca3512b
Cucco2001 <giancarlo.cuccorese@gmail.com>
2022-12-02 12:05:05 +0100

    restored previous check_if_obstacle

--- ComputeTransitionProbabilities.py
+++ ComputeTransitionProbabilities.py
@@ -2,9 +2,10 @@ import numpy as np
 import scipy
 from Constants import *
 
-def check_if_obstacle(b,m1,n1):
-      if np.any(np.equal([m1,n1],b.T).all(axis=1)):
-            return True
+def check_if_obstacle(m,n,m1,n1):
+      for i in range(0,len(m)):
+            if m[i]==m1 and n[i]==n1:
+                  return True
       return False
 def check_if_portal(m,n,m1,n1):
       for i in range(0,len(m)):
@@ -38,48 +39,41 @@ def near_alien(m,n,m1,n1,psi):
             if m1==m[i] and n1==n[i]:
                   num=num+1
       return num
-
 def in_mine(m, n, m1, n1, psi1):
       if m == m1 and n == n1 and psi1 == Constants.LOWER:
             return True
       return False
-def not_accessible(b,m2,n2):
+def not_accessible(m,n,m2,n2):
       if m2 == Constants.M or m2 == -1 or n2 == Constants.N or n2 == -1:
             return True
-      return check_if_obstacle(b,m2,n2)
+      return check_if_obstacle(m,n,m2,n2)
 
 
 def ComputeTransitionProbabilities(stateSpace, map_world, K):
     """
     Computes the transition probabilities between all states in the state space for
     all control inputs.
-
     @type  stateSpace: (K x 4)-matrix
     @param stateSpace: Matrix where the i-th row represents the i-th
           element of the state space.
-
     @type  map_world: (M x N)-matrix
     @param  map_world:      A matrix describing the terrain.
           With values: FREE OBSTACLE PORTAL ALIEN MINE
           LAB BASE
-
     @type  K: integer
     @param K: An integer representing the total number of states in the state space
-
     @return P:
               A (K x K x L)-matrix containing the transition probabilities
               between all states in the state space for all control inputs.
               The entry P(i, j, l) represents the transition probability
               from state i to state j if control input l is applied.
-
     """
-      
+
     m_lab,n_lab = np.where(map_world == Constants.LAB)
     m_base,n_base = np.where(map_world == Constants.BASE)
     m_portal,n_portal = np.where(map_world == Constants.PORTAL) 
     m_mine,n_mine= np.where(map_world == Constants.MINE)        
     m_obstacle,n_obstacle = np.where(map_world == Constants.OBSTACLE)
-    obstacle_matrix = np.array([m_obstacle,n_obstacle])
     m_alien,n_alien = np.where(map_world == Constants.ALIEN)
     i_terminal = np.where((stateSpace == np.array([m_lab,n_lab,1,0])).all(axis=1))[0][0]
     P_DISTURBED = Constants.P_DISTURBED
@@ -97,7 +91,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
       Prb[(i,j_base,azione)] = 0
 
       if north:
-            if not not_accessible(obstacle_matrix, m_arriv, n_arriv+1):    
+            if not not_accessible(m_obstacle, n_obstacle, m_arriv, n_arriv+1):    
                   port = check_if_portal(m_portal, n_portal, m_arriv, n_arriv+1)        #1a
                   psi_end = psi_arriv*(1-port) + (1-psi_arriv)*port
                   phi_end = phi_arriv
@@ -125,7 +119,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
                   Constants.cost_dict[(i,azione)] = Constants.cost_dict[(i,azione)] + Constants.N_b*(1-(1-S)*psi_arriv)*P_DISTURBED/3
 
       if not north:
-            if not not_accessible(obstacle_matrix, m_arriv, n_arriv-1):    #south
+            if not not_accessible(m_obstacle, n_obstacle, m_arriv, n_arriv-1):    #south
                   port = check_if_portal(m_portal, n_portal, m_arriv, n_arriv-1)        #1a
                   psi_end = psi_arriv*(1-port) + (1-psi_arriv)*port
                   phi_end = phi_arriv
@@ -153,7 +147,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
                   Constants.cost_dict[(i,azione)] = Constants.cost_dict[(i,azione)] + Constants.N_b*(1-(1-S)*psi_arriv)*P_DISTURBED/3
             
       
-      if not not_accessible(obstacle_matrix, m_arriv+1, n_arriv):    #right
+      if not not_accessible(m_obstacle, n_obstacle, m_arriv+1, n_arriv):    #right
             port = check_if_portal(m_portal, n_portal, m_arriv+1, n_arriv)        #1a
             psi_end = psi_arriv*(1-port) + (1-psi_arriv)*port
             phi_end = phi_arriv
@@ -180,7 +174,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
             Prb[(i,j_base,azione)] = Prb[(i,j_base,azione)] + ((1-(1-S)*psi_arriv)*P_DISTURBED/3)*p_prec
             Constants.cost_dict[(i,azione)] = Constants.cost_dict[(i,azione)] + Constants.N_b*(1-(1-S)*psi_arriv)*P_DISTURBED/3
 
-      if not not_accessible(obstacle_matrix, m_arriv-1, n_arriv):      #left
+      if not not_accessible(m_obstacle, n_obstacle, m_arriv-1, n_arriv):      #left
             port = check_if_portal(m_portal, n_portal, m_arriv-1, n_arriv)        #1a
             psi_end = psi_arriv*(1-port) + (1-psi_arriv)*port
             phi_end = phi_arriv
@@ -241,7 +235,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
 
 
       j = np.where((stateSpace == np.array([m_b,n_b,phi_b,psi_b])).all(axis=1))[0][0]
-      Prob[(i,j,act)] = ((1-(1-S)*psi_b)*P_DISTURBED)*((P_PROTECTED**num_alien)*(1+phi2_lotta) - (2+phi2_lotta)*phi2_lotta)   #prob of fighting or not and not disturbed
+      Prob[(i,j,act)] = (1-(1-(1-S)*psi_b)*P_DISTURBED)*((P_PROTECTED**num_alien)*(1+phi2_lotta) - (2+phi2_lotta)*phi2_lotta)   #prob of fighting or not and not disturbed
       Prob_dist = move_disturbed(m_b, n_b, psi_b, phi_b, act, ((P_PROTECTED**num_alien)*(1+phi2_lotta) - (2+phi2_lotta)*phi2_lotta),False)
       for key in Prob_dist.keys():
             if key not in Prob.keys():
@@ -261,7 +255,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
       if i != i_terminal:           # if not in terminal state then execute
             #South
             action = Constants.SOUTH
-            if not up and not not_accessible(obstacle_matrix, m1, n1-1):
+            if not up and not not_accessible(m_obstacle, n_obstacle, m1, n1-1):
                   m2 = m1
                   n2 = n1-1 
                   Prob_move = move_algorithm(m2,n2,action,psi1,phi1)
@@ -269,7 +263,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
                         P[key[0], key[1], key[2]] = P[key[0], key[1], key[2]] + Prob_move[key]
 
             action = Constants.NORTH
-            if up and not not_accessible(obstacle_matrix, m1, n1+1):
+            if up and not not_accessible(m_obstacle, n_obstacle, m1, n1+1):
                   m2 = m1
                   n2 = n1+1
                   Prob_move = move_algorithm(m2,n2,action,psi1,phi1)
@@ -277,7 +271,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
                         P[key[0], key[1], key[2]] = P[key[0], key[1], key[2]] + Prob_move[key]
 
             action = Constants.EAST
-            if not not_accessible(obstacle_matrix, m1+1, n1):
+            if not not_accessible(m_obstacle, n_obstacle, m1+1, n1):
                   m2 = m1+1
                   n2 = n1
                   Prob_move = move_algorithm(m2,n2,action,psi1,phi1)
@@ -285,7 +279,7 @@ def ComputeTransitionProbabilities(stateSpace, map_world, K):
                         P[key[0], key[1], key[2]] = P[key[0], key[1], key[2]] + Prob_move[key]
 
             action = Constants.WEST
-            if not not_accessible(obstacle_matrix, m1-1, n1):
+            if not not_accessible(m_obstacle, n_obstacle, m1-1, n1):
                   m2 = m1-1
                   n2 = n1
                   Prob_move = move_algorithm(m2,n2,action,psi1,phi1)
